6633848d08d4d4e540c887d7e1408e02049e9700
DiabloHorn <diablohorn@gmail.com>
2020-03-28 05:39:55 -0700

    added comments, reverted firewall commit

--- setup-config.sh
+++ setup-config.sh
@@ -56,7 +56,7 @@ set system ntp server '2.pool.ntp.org'
 commit
 echo "[V] NTP configured"
 
-######## NTP
+######## DNS
 echo "[*] configuring DNS"
 set system name-server '8.8.8.8'
 set system name-server '8.8.4.4'
@@ -66,11 +66,13 @@ echo "[V] DNS configured"
 ######## Firewall
 echo "[*] configuring firewall"
 
+######## global states
 set firewall state-policy established action accept
 set firewall state-policy related action accept
 set firewall state-policy invalid action drop
 set firewall source-validation strict
 
+######## define groups
 set firewall group network-group internalranges
 set firewall group network-group internalranges network '10.0.0.0/8'
 set firewall group network-group internalranges network '172.16.0.0/12'
@@ -86,11 +88,12 @@ set firewall group address-group dns-servers address '8.8.4.4'
 set firewall group address-group dns-servers description 'Google DNS'
 commit
 
+######## define zone policies
 set zone-policy zone local local-zone
 set zone-policy zone local default-action drop
 
 set zone-policy zone uplink
-set zone-policy zone uplink interface eth0t
+set zone-policy zone uplink interface eth0
 set zone-policy zone uplink default-action drop
 set zone-policy zone uplink description 'uplink zone'
 
@@ -107,6 +110,7 @@ commit
 
 save
 
+######## configure firewall rules per zone
 set firewall name uplinkTOinetonly default-action drop
 set firewall name uplinkTOmgmt default-action drop
 commit
@@ -134,11 +138,14 @@ commit
 
 set firewall name inetonlyTOmgmt default-action drop
 
-set firewall name inetonlyTOuplink rule 10 action accept
+set firewall name inetonlyTOuplink default-action accept
+set firewall name inetonlyTOuplink rule 10 action drop
 set firewall name inetonlyTOuplink rule 10 protocol tcp_udp
-set firewall name inetonlyTOuplink rule 10 destination group network-group ! internalranges
+set firewall name inetonlyTOuplink rule 10 destination group network-group internalranges
+
 commit
 
+######## apply rules to zones
 set zone-policy zone uplink from inetonly firewall name inetonlyTOuplink
 set zone-policy zone uplink from mgmt firewall name mgmtTOuplink
 set zone-policy zone uplink from local firewall name localTOuplink
